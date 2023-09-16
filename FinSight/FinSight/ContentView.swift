e09889aba4ca170c9164106a71ef022170564df4
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2023-09-16 12:38:07 -0400

    XCode Proj init

---
+++ FinSight/FinSight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinSight
+//
+//  Created by Yunseo Lee on 9/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinSight/FinSight/FinSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinSightApp.swift
+//  FinSight
+//
+//  Created by Yunseo Lee on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FinSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
