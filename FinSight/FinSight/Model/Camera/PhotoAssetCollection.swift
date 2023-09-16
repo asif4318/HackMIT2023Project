11fbc3d6c53ab91462fbe3e19ab5835273c1b1e1
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2023-09-16 16:16:55 -0400

    Initial Camera Feed

---
+++ FinSight/FinSight/Model/Camera/PhotoAssetCollection.swift
@@ -0,0 +1,54 @@
+//
+//  PhotoAssetCollection.swift
+//  FinSight
+//
+//  Created by Yunseo Lee on 9/16/23.
+//
+
+import Photos
+
+class PhotoAssetCollection: RandomAccessCollection {
+    private(set) var fetchResult: PHFetchResult<PHAsset>
+    private var iteratorIndex: Int = 0
+    
+    private var cache = [Int : PhotoAsset]()
+    
+    var startIndex: Int { 0 }
+    var endIndex: Int { fetchResult.count }
+    
+    init(_ fetchResult: PHFetchResult<PHAsset>) {
+        self.fetchResult = fetchResult
+    }
+
+    subscript(position: Int) -> PhotoAsset {
+        if let asset = cache[position] {
+            return asset
+        }
+        let asset = PhotoAsset(phAsset: fetchResult.object(at: position), index: position)
+        cache[position] = asset
+        return asset
+    }
+    
+    var phAssets: [PHAsset] {
+        var assets = [PHAsset]()
+        fetchResult.enumerateObjects { (object, count, stop) in
+            assets.append(object)
+        }
+        return assets
+    }
+}
+
+extension PhotoAssetCollection: Sequence, IteratorProtocol {
+
+    func next() -> PhotoAsset? {
+        if iteratorIndex >= count {
+            return nil
+        }
+        
+        defer {
+            iteratorIndex += 1
+        }
+        
+        return self[iteratorIndex]
+    }
+}
