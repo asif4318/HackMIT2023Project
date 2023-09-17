22e413e8864a2c609830698da060227900560d0f
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2023-09-17 03:38:36 -0400

    Added SiriKit

---
+++ FinSight/FinSightIntent/FinSightIntentHandler.swift
@@ -0,0 +1,18 @@
+//
+//  FinSightIntentHandler.swift
+//  FinSight
+//
+//  Created by Yunseo Lee on 9/17/23.
+//
+
+import Foundation
+
+class FinSightIntentHandler: NSObject, FinsightIntentIntentHandling{
+    func confirm(intent: FinsightIntentIntent, completion: @escaping (FinsightIntentIntentResponse) -> Void) {
+        
+    }
+    
+    func handle(intent: FinsightIntentIntent, completion: @escaping (FinsightIntentIntentResponse) -> Void) {
+        
+    }
+}
