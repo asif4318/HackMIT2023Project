70df1127cc0e4719490e2060b6807503df55d9a5
Asif Islam <77113884+asif4318@users.noreply.github.com>
2023-09-17 03:38:54 -0400

    Swift - added function to analyze receipts

--- FinSight/FinSight/Model/Camera/DataModel.swift
+++ FinSight/FinSight/Model/Camera/DataModel.swift
@@ -14,6 +14,7 @@ final class DataModel: ObservableObject {
     let camera = Camera()
     let photoCollection = PhotoCollection(smartAlbum: .smartAlbumUserLibrary)
     let ocr = OCRViewModel()
+    let api = LLM_API()
     
     @Published var viewfinderImage: Image?
     @Published var thumbnailImage: Image?
@@ -61,6 +62,8 @@ final class DataModel: ObservableObject {
                 if let text = recognizedText, !text.isEmpty {
                     // Store the recognized text or process it further
                     self.recognizedText = text
+                    let output = self.api.analyzeReceipt(receipt_data: self.recognizedText)
+                    print(output)
                 }
                 print(self.recognizedText)
             }
